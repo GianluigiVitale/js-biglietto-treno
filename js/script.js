bd658a4d8fb44444346f20e3a8ea5dcbcb09034d
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-02-18 17:02:32 +0100

    instead of >65  >=65

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ var costoPerOver65 = costoChilometriTotale * 0.6;
 
 if (anni < 18) {
     document.getElementById('costobiglietto').innerHTML = ('Prezzo: ' + Math.ceil(costoPerMinorenni) + '€');
-} else if (anni > 65) {
+} else if (anni >= 65) {
     document.getElementById('costobiglietto').innerHTML = ('Prezzo: ' + Math.ceil(costoPerOver65) + '€');
 } else {
     document.getElementById('costobiglietto').innerHTML = ('Prezzo: ' + Math.ceil(costoChilometriTotale) + '€');
