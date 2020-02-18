7edc3787595a4ca6dc7d01f7204108d7c4b4b810
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-02-18 15:51:04 +0100

    added ;

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
-var chilometri = prompt('Quanti chilometri vuoi percorrere?')
-var anni = prompt('Quanti anni hai?')
-var costoPerChilometro = 0.21
-var costoChilometriTotale = chilometri * costoPerChilometro
-var costoPerMinorenni = chilometri * costoPerChilometro * 0.8
-var costoPerOver65 = chilometri * costoPerChilometro * 0.6
+var chilometri = prompt('Quanti chilometri vuoi percorrere?');
+var anni = prompt('Quanti anni hai?');
+var costoPerChilometro = 0.21;
+var costoChilometriTotale = chilometri * costoPerChilometro;
+var costoPerMinorenni = chilometri * costoPerChilometro * 0.8;
+var costoPerOver65 = chilometri * costoPerChilometro * 0.6;
 
 if (anni < 18) {
     document.getElementById('costobiglietto').innerHTML = ('Prezzo: ' + Math.ceil(costoPerMinorenni) + '€');
