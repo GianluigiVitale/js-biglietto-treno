57feac9887122d62a44d541f645bd9ab2fa782cb
Gianluigi Vitale <gianluigi.vitale11@gmail.com>
2020-02-18 16:59:25 +0100

    added comment to js and improved js code

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
+/*
+Il programma dovrà chiedere all’utente il numero di chilometri che vuole
+percorrere e l’età del passeggero. Sulla base di queste informazioni dovrà
+calcolare il prezzo totale del viaggio. Il prezzo del biglietto è definito in base ai
+km (0.21 € al km), ma va applicato uno sconto del 20% per i minorenni e del 40% per gli over 65.
+
+1. 2 Variabili con prompt per chiedere il 'numero di chilometri' e 'l'età'
+2. Variabile con 'costo per chilometro'
+3. Variabile con 'costo per chilometro' moltiplicata per il 'numero di chilometri'
+4. Variabile con sconto per minorenni
+5. Variabile con sconto per over65
+6. Creo un if - else if - else dove se l'eta inserita e' minore di 18 nell'html viene inserito il costo per costoPerMinorenni
+    se over 65 costo per over65 altrimenti prezzo normale
+*/
+
 var chilometri = prompt('Quanti chilometri vuoi percorrere?');
 var anni = prompt('Quanti anni hai?');
 var costoPerChilometro = 0.21;
 var costoChilometriTotale = chilometri * costoPerChilometro;
-var costoPerMinorenni = chilometri * costoPerChilometro * 0.8;
-var costoPerOver65 = chilometri * costoPerChilometro * 0.6;
+var costoPerMinorenni = costoChilometriTotale * 0.8;
+var costoPerOver65 = costoChilometriTotale * 0.6;
 
 if (anni < 18) {
     document.getElementById('costobiglietto').innerHTML = ('Prezzo: ' + Math.ceil(costoPerMinorenni) + '€');
